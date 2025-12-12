669fc7237dbf2eb439680f99c898b6af499114ad
josesantosra92-collab <josesantosra.92@gmail.com>
2025-12-11 22:05:05 -0700

    Update content paths in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
- content: ["./index.html", "./**/*.{js,jsx,ts,tsx}"],
+content: ["./index.html", "./pages/**/*.{js,jsx}", "./App.jsx"],
   theme: {
     extend: {},
   },
