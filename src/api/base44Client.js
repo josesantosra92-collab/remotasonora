a5edea0f57d74823669e9395736f0dd3f3af574a
josesantosra92-collab <josesantosra.92@gmail.com>
2025-12-16 17:58:41 -0700

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,1 +1,6 @@
+import { Base44Client } from '@base44/sdk';
 
+export const base44 = new Base44Client({
+  appId: import.meta.env.VITE_BASE44_APP_ID,
+  apiKey: import.meta.env.VITE_BASE44_API_KEY,
+});
