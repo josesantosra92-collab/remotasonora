3d6383cf439f88e6c7bc44d25bd5f408878d4ce4
josesantosra92-collab <josesantosra.92@gmail.com>
2025-12-16 17:57:06 -0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,8 @@ export function createPageUrl(pageName) {
   const routes = {
     'Home': '/',
     'Privacy': '/privacy',
-    'Terms': '/terms'
+    'Terms': '/terms',
+    'ClientPortal': '/portal'
   }
   return routes[pageName] || '/'
 }
